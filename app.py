3da24562a24ad1ac66157d45dde5a8185a2a8307
gbzdy0416 <sareodcnp@gmail.com>
2025-01-12 22:39:57 +0100

    1

--- app.py
+++ app.py
@@ -1,16 +1,10 @@
 from flask import Flask, redirect, request, render_template
-import bcrypt
 import os
-import string
-import subprocess
-import socket
+from pickledb import PickleDB
 
 app = Flask(__name__)
 
-if not os.path.exists("app-secret.key"):
-    with open("app-secret.key", "wb") as f:
-        f.write(b"name and password inside:\n")
-
+db = PickleDB("t.db")
 
 @app.route('/')
 def hello_world():  # put application's code here
@@ -22,19 +16,16 @@ def login():
     if request.method == "POST":
         username = request.form["username"]
         pw = request.form["password"]
-        with open("app-secret.key", "wb") as f:
-            f.write((username + "\n" + pw).encode())
+        db.set('name', username)
+        db.set('password', pw)
+        db.save()
     return render_template("tmp.html")
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0", port=8080)
 
 
 @app.route("/check")
 def check():
-    if not os.path.exists("app-secret.key"):
-        return render_template("check.html", ln="invalid!")
-    with open("app-secret.key", "rb") as f:
-        things = f.read()
-    return render_template("check.html", ln=things)
+    return render_template("check.html", name = db.get('name'), password=db.get('password'))
