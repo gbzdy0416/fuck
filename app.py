3c483b96fbb367db900729ca9afc8248c23c7e6f
gbzdy0416 <sareodcnp@gmail.com>
2025-01-12 22:46:57 +0100

    1

--- app.py
+++ app.py
@@ -21,11 +21,9 @@ def login():
         db.save()
     return render_template("tmp.html")
 
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8080)
-
-
 @app.route("/check")
 def check():
     return render_template("what.html", name = db.get('name'), password=db.get('password'))
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8080)
